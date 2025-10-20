df5c4d4b65090f32836dfc662e9556c67fd6e8a9
Magdalena Anodalieva <manodali@uoguelph.ca>
2025-10-20 12:21:28 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -2,6 +2,10 @@
 
 # Word frequency exercise
 # TODO: (Read detailed instructions in the Readme file)
+# 1. Prompt the user: Ask the user to enter a sentence.
+# 2. Split the sentence
+# 3. Create lists to store words and their corresponding frequencies.
+# 4. Iterate through words and update frequencies
 
 import re
 
@@ -24,3 +28,28 @@ def is_sentence(text):
         return False
 
     return True
+
+user_sentence = input("Enter a sentence: ")
+
+while (is_sentence(user_sentence) == False):
+    print("This does not meet the criteria for a sentence.")
+    user_sentence = input("Enter a sentence: ")   
+
+
+words = user_sentence.split()
+
+unique_words = []
+frequencies = []
+
+for word in words:
+    word = word.strip(".,!?").lower() 
+    if word in unique_words:
+        index = unique_words.index(word)
+        frequencies[index] += 1
+    else:
+        unique_words.append(word)
+        frequencies.append(1)
+
+print("\nWord Frequencies:")
+for i in range(len(unique_words)):
+    print(f"{unique_words[i]}: {frequencies[i]}")
